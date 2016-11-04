0128ff8f6748526bdb17e8fff42ff339cc62bc4d
Aleena Tucker <Aleena@DESKTOP-N857AIT>
2016-11-04 14:30:40 -0400

    Add project files.

---
+++ LabNumber9/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LabNumber9
+{
+    internal class Circle
+    {
+        private readonly double pi = Math.PI;
+        public double radius;
+
+        public Circle(double radius) //constructor
+        {
+            this.radius = radius;
+        }
+
+        public double getCircumference
+        {
+            get { return 2*radius*pi; }
+        }
+
+        public double getArea
+        {
+            get { return pi*(radius*radius); }
+        }
+
+        public string getFormattedCircumference()
+        {
+            return Math.Round(getCircumference, 2).ToString();
+        }
+
+        public string getFormattedArea()
+        {
+            return Math.Round(getArea, 2).ToString();
+        }
+    }
+}
---
+++ LabNumber9/CircleApp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LabNumber9
+{
+    internal class CircleApp
+    {
+        private static void Main(string[] args)
+        {
+            string radius, cont = "y";
+            var count = 0;
+            Console.WriteLine("Welcome to the circle tester!");
+
+            while (cont == "y")
+            {
+                count = count + 1;
+                Console.Write("Enter radius: ");
+                radius = Console.ReadLine();
+                var circle1 = new Circle(double.Parse(radius));
+                Console.WriteLine();
+
+                Console.WriteLine("Circumference: " + circle1.getFormattedCircumference());
+                Console.WriteLine("Area: " + circle1.getFormattedArea());
+                Console.WriteLine();
+
+                Console.Write("Continue? (y/n) ");
+                cont = Console.ReadLine().ToLower();
+                switch (cont)
+                {
+                    case "n":
+                        Console.WriteLine();
+                        break;
+                    case "y":
+                        Console.WriteLine();
+                        continue;
+                    default:
+                        Console.WriteLine("Invalid entry, try again.");
+                        //input validation since there are only 2 right answers
+                        cont = Console.ReadLine();
+                        break;
+                }
+                while (cont != "yes" && cont != "y" && cont != "no" && cont != "n")
+                {
+                    Console.Write("Invalid entry. Try again! "); //input validation since there are only 2 right answers
+                    cont = Console.ReadLine();
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Goodbye. You created " + count + " Circle object(s).");
+        }
+    }
+}
